3bc6c8e5744657f7141047f260aaf113902e6226
Christopher Willard <cwillard@carqualifier.com>
2016-07-14 13:49:46 -0400

    Update

--- orient_service.sh
+++ orient_service.sh
@@ -3,11 +3,9 @@
 case $1 in
   start)
      /orientdb/bin/dserver.sh &
-     echo $! > /var/run/start_orient.pid ;
      ;;
    stop)
      /orientdb/bin/shutdown.sh
-     rm /var/run/start_orient.pid
      ;;
    *)
      echo "usage: orient_service.sh {start|stop}" ;;
