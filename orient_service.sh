029396273e2c1e56e99305e997e3f290bf1196dd
Christopher Willard <cwillard@carqualifier.com>
2016-08-16 13:45:10 -0400

    Update

--- orient_service.sh
+++ orient_service.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 
 case $1 in
   start)
--- run.sh
+++ run.sh
@@ -3,5 +3,4 @@
 touch /data/status/orient.ready
 chmod 775 /data/status/orient.ready
 
-tail -f /dev/null
-#/opt/monit/bin/monit -I
+/opt/monit/bin/monit -I
